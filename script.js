a21b4613479cb34ec48c8a7ff2ead544bdc7d94b
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-13 17:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function shareurl(){
+	let name = document.querySelector("#name").value;
+	let year = document.querySelector("#year").value;
+	let url = "https://localhost:8080/?";
+	if(name.trim().length!=0){
+		url += "name="+name;
+		if(year.trim().length!=0){
+		url += "&";
+	}
+	}
+	if(year.trim().length!=0){
+		url += "year="+year;
+	}
+	// let url = "https://localhost:8080/?name="+name+"&year="+year;
+	document.querySelector("#url").innerText = url;
+}
